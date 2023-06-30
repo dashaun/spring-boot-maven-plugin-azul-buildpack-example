ddf5978c3c9cb64245080f0a63ed5f98b3e64a70
DaShaun <dashaun@dashaun.com>
2023-06-30 22:48:38 +0000

    Quick demo for Frank Delporte

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# spring-boot-maven-plugin-azul-buildpack-example
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+
+# Spring Boot Maven Plugin Azul Buildpack Example
+
+## Prerequisites
+- [Demo Magic](https://github.com/paxtonhare/demo-magic) needs to be in the path
+- Java
+- [Initializer-plusplus](https://github.com/dashaun/initializr-plusplus) needs to be in the path
+
+## Quick Start
+```bash
+./demo.sh
+```
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[forks-shield]: https://img.shields.io/github/forks/dashaun/spring-boot-maven-plugin-azul-buildpack-example.svg?style=for-the-badge
+[forks-url]: https://github.com/dashaun/spring-boot-maven-plugin-azul-buildpack-example/forks
+[stars-shield]: https://img.shields.io/github/stars/dashaun/spring-boot-maven-plugin-azul-buildpack-example.svg?style=for-the-badge
+[stars-url]: https://github.com/dashaun/spring-boot-maven-plugin-azul-buildpack-example/stargazers
+[issues-shield]: https://img.shields.io/github/issues/dashaun/spring-boot-maven-plugin-azul-buildpack-example.svg?style=for-the-badge
+[issues-url]: https://github.com/dashaun/spring-boot-maven-plugin-azul-buildpack-example/issues
---
+++ demo.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+. demo-magic.sh
+export TYPE_SPEED=100
+export DEMO_PROMPT="${GREEN}➜ ${CYAN}\W ${COLOR_RESET}"
+TEMP_DIR=upgrade-example
+
+function talkingPoint() {
+  wait
+  clear
+}
+
+function initSDK() {
+  source "$HOME/.sdkman/bin/sdkman-init.sh"
+}
+
+function createAppWithInitializr {
+  # hide the evidence
+  rm -rf $TEMP_DIR
+  mkdir $TEMP_DIR
+  cd $TEMP_DIR || exit
+  clear
+  pei "sdk use java 17.0.7-zulu"
+  pei "java -version"
+  pei "curl https://start.spring.io/starter.tgz -d dependencies=web,actuator -d javaVersion=17 -d bootVersion=3.1.1 -d type=maven-project | tar -xzf - || exit"
+}
+
+function zuluBuilder {
+  git init && git add . && git commit -m 'initializr'
+  pei "initializr-plusplus extensions"
+  pei "initializr-plusplus project-version"
+  git add . && git commit -m 'extensions and project-version'
+  clear
+  pei "initializr-plusplus zulu-builder"
+  pei "git --no-pager diff pom.xml"
+  git add . && git commit -m 'zulu-builder'
+}
+
+function springBootBuildImage {
+  pei "./mvnw -q spring-boot:build-image -DskipTests"
+}
+
+function validateImage {
+  pei "docker run --name zuluImageExample --rm -d -p 8080:8080 demo:0.0.0"
+  pei "docker stats --no-stream zuluImageExample"
+  pei "sleep 5"
+  pei "http :8080/actuator/health"
+  pei "docker exec zuluImageExample cat gc.log"
+}
+
+function cleanupImage {
+  docker stop zuluImageExample
+  docker rmi demo:0.0.0
+  clear
+}
+
+initSDK
+createAppWithInitializr
+talkingPoint
+zuluBuilder
+talkingPoint
+springBootBuildImage
+validateImage
+talkingPoint
+cleanupImage
